66f4a0f0f8f94c212286ef499ee488ea0dd7eab0
Hju Kneyck Flores <hjucode@gmail.com>
2022-11-12 23:52:18 +0800

    feat: enable cors

--- app/main.py
+++ app/main.py
@@ -3,9 +3,18 @@ import json
 
 import pandas
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class ExcelFile(BaseModel):
